9e03ed6d3f7c87e1121c9e95b933e9e5b3d8f38a
Yudha Pratama Putra <yp021004@gmail.com>
2021-10-02 22:51:32 -0700

    second commit

---
+++ lib/flutter_dha_package.dart
@@ -0,0 +1,3 @@
+library flutter_dha_package;
+
+export 'global_widget/error_get_data_widget.dart';
---
+++ lib/global_widget/error_get_data_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ErrorGetDataWidget extends StatelessWidget {
+  const ErrorGetDataWidget({Key? key, required this.msg}) : super(key: key);
+  final String msg;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          '$msg',
+          style: TextStyle(
+            color: Colors.orange,
+          ),
+        ),
+      ),
+    );
+  }
+}
