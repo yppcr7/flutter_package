89e3366dfc5c7287a3a077883d221a9369da131e
Yudha Pratama Putra <yp021004@gmail.com>
2021-10-02 23:41:53 -0700

    edit constatns class

--- lib/flutter_dha_package.dart
+++ lib/flutter_dha_package.dart
@@ -2,3 +2,4 @@ library flutter_dha_package;
 
 export 'global_widget/loading_widget.dart';
 export 'global_widget/error_get_data_widget.dart';
+export 'utils/constatns.dart';
--- lib/utils/constatns.dart
+++ lib/utils/constatns.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-Text customWidget(
+Text customText(
   String text,
   Color textColor,
   double textSize,
